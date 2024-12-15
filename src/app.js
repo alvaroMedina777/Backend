f01047f5dff1838ff11bfc8fab7f58791c2f938f
alvaroMedina777 <alvaromed458@gmail.com>
2024-12-15 18:19:59 -0300

    Creacion de las consultas

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import loginRoutes from './routes/login.routes.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import consultaRoutes from './routes/consultas.routes.js';
 
 const app = express();
 app.use(express.json());
@@ -12,6 +13,7 @@ app.use(cookieParser());
 app.use(cors(
     {
         origin: 'http://localhost:5173',
+        credentials: true,
         // methods: ['GET', 'POST', 'PUT', 'DELETE'],
         // allowedHeaders: ['Content-Type', 'Authorization']
         
@@ -20,5 +22,9 @@ app.use(cors(
 ));
 
 app.use('/api/usuarios', loginRoutes);
+
+//////////////////
+app.use('/api/consulta', consultaRoutes);
+/////////////
 
 export default app;
